ce625a9496b93cd44abbf0bd11c4408e6a40df38
Fumblepuff <jhkalu@gmail.com>
2022-08-07 14:12:15 -0400

    created cap count script

---
+++ level_2/capCount.py
@@ -0,0 +1,34 @@
+import sys
+
+for i in sys.argv[1:]:
+    #Taking in the argument as a string
+    input = str(i)
+    
+    #This variable will hold the count of capital letters
+    count = 0
+    
+    #This list will hold the indices of the capital letters
+    indices = []
+    
+    #This variable will hold the total of the indices
+    total = 0
+    
+    #Iterating through the string
+    for j in input:
+        #If the character is upercase, increment the count
+        if j.isupper():
+            count += 1
+            #Appending the index of the character to the list
+            indices.append(input.index(j))
+    
+    
+    #Iterating through the list of indices
+    for k in indices:
+        #Adding the index to the total
+        total += k
+    
+    
+    #Printing the count of capital letters
+    print(count)
+    #Printing the added indices of the capital letters
+    print(total)
