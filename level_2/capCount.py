c5ee9381324dd3ac97993193129974081ba59d39
Fumblepuff <jhkalu@gmail.com>
2022-08-09 08:35:45 -0400

    removed indices list

--- level_2/capCount.py
+++ level_2/capCount.py
@@ -7,9 +7,6 @@ for i in sys.argv[1:]:
     #This variable will hold the count of capital letters
     count = 0
     
-    #This list will hold the indices of the capital letters
-    indices = []
-    
     #This variable will hold the sum of the indices
     sum = 0
     
@@ -18,14 +15,9 @@ for i in sys.argv[1:]:
         #If the character is upercase, increment the count
         if j.isupper():
             count += 1
-            #Appending the index of the character to the list
-            indices.append(input.index(j))
+            #Incrementing the sum by the index of the character
+            sum += input.index(j)
     
-    
-    #Iterating through the list of indices
-    for k in indices:
-        #Adding the index to the sum
-        sum += k
     
     
     #Printing the count of capital letters
